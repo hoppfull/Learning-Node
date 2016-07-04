1400f38970e1385d19d1a045a13b0082152e9862
hoppfull <hoppfull@gmail.com>
2016-07-04 12:48:07 +0200

    added 404 response logic and index.html to ex01

--- Basics/ex01/src/main.ts
+++ Basics/ex01/src/main.ts
@@ -1,11 +1,14 @@
 import http = require('http')
+import fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end("tjening\n")
-})
-
-server.listen(3000, '127.0.0.1', () => {
-    console.log("Server is listening...")
-})
+http.createServer((request, response) => {
+    console.log(`Request recieved:\n\ttype:\t${request.method}\n\turl:\t\'${request.url}\'`)
+    if (request.method === 'GET' && request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        fs.createReadStream('src/index.html').pipe(response)
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain' })
+        response.write("Error 404: Page not found!")
+        response.end()
+    }
+}).listen(8000, () => { console.log("Server is listening...") })
